fbfa5550cd27a1d95d877185a8fa880622a68495
rahimahgilani <rahimahgilani@gmail.com>
2026-06-14 11:26:31 +0500

    Shifted Analyze Function Code to Parser.py

---
+++ parser.py
@@ -0,0 +1,28 @@
+import json
+import os
+from groq import Groq
+from prompts import SYSTEM_PROMPT
+
+def analyse_expenses(expenses_text):
+    # user --> the text they enter 
+    # system --> the prompt that guides the model on how to analyse the expenses and what output format to use
+    response = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {"role": "system", "content": SYSTEM_PROMPT},
+            {"role": "user", "content": expenses_text}
+        ]
+    )
+    
+    # the model's response will be in the format specified in the system prompt, but we still need to clean it and convert it from a string to a JSON object
+    raw = response.choices[0].message.content
+    
+    # clean response in case model adds anything before/after JSON
+    raw = raw.strip()
+    if raw.startswith("```"):
+        raw = raw.split("```")[1]
+        if raw.startswith("json"):
+            raw = raw[4:]
+    
+    # convert json to dict
+    return json.loads(raw) 
