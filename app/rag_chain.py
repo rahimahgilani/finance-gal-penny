ea71cddcedb73c48ef5b7e8e65c54865f134f4ab
rahimahgilani <rahimahgilani@gmail.com>
2026-07-20 15:58:59 +0500

    Setting up Supabase to connect to Postgres DB

---
+++ app/rag_chain.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain.messages import HumanMessage
+
+load_dotenv()
+
+def rag_chain_pipeline(vector_store, question, filename):
+    # Step 1: Querying the collection by turning it into a retriever
+    retriever = vector_store.as_retriever(search_kwargs={"filter": {"source": filename}})
+
+    text = retriever.invoke(question)
+
+    # print(f"Retrieved text: {text}")
+    # print(f"Number of retrieved documents: {len(text)}")
+
+    # Step 2: Convert the retrieved documents into a single string
+    singleString = " ".join([item.page_content for item in text])
+
+    # print(f"Single string: {singleString}")
+
+    # Step 3: Setting up ChatGroq
+    llm = ChatGroq(model="meta-llama/llama-4-scout-17b-16e-instruct")
+
+    # Step 4: Building a simple prompt template
+    message = HumanMessage(
+        content=[
+            {"type": "text", "text": singleString},
+            {"type": "text", "text": question},
+        ]
+    )
+
+    response = llm.invoke([message])
+    return response.content
---
+++ app/vector_store.py
@@ -0,0 +1,31 @@
+import os
+from langchain_chroma import Chroma
+from app.embeddings import get_embeddings
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+CHROMA_DIR = os.path.join(BASE_DIR, "data", "chroma_db")
+
+def create_vector_store(chunks, filename):
+    # print(f"Using chroma dir: {CHROMA_DIR}")
+    embeddings = get_embeddings()
+    
+    vector_store = Chroma(
+        collection_name="pdf-embeddings",
+        embedding_function=embeddings,
+        persist_directory=CHROMA_DIR,
+    )
+    
+    vector_store.add_texts(chunks, metadatas=[{"source": filename} for _ in chunks])
+    
+    return vector_store
+
+def load_vector_store():
+    # print(f"Using chroma dir: {CHROMA_DIR}")
+    embeddings = get_embeddings()
+    vector_store = Chroma(
+        collection_name="pdf-embeddings",
+        embedding_function=embeddings,
+        persist_directory=CHROMA_DIR,
+    )
+    return vector_store
+
